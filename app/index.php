9e72662e1b7fee9b8897ea36b11c641f925f3469
Wisley Alves <33531578+wisley7l@users.noreply.github.com>
2018-01-08 15:23:14 -0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -3,5 +3,10 @@ header('Content-Type: text/html; charset=utf-8');
 
 // sample only
 echo Addons\MKTP_TITLE;
+echo PHP_EOL;
+echo $_SERVER['PATH_FILE'];
+echo PHP_EOL;
+echo $_SSERVER['PATH_LANG'];
+echo PHP_EOL;
 
 // TODO: parse twig template
