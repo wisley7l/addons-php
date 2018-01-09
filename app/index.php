7895782e0593111100a2e4856e663336793a70c2
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-09 10:32:27 -0200

    treating menu in index

--- app/index.php
+++ app/index.php
@@ -1,7 +1,17 @@
 <?php
+require_once "../composer/vendor/autoload.php";
 header('Content-Type: text/html; charset=utf-8');
-
-// sample only
-echo Addons\MKTP_TITLE;
-
 // TODO: parse twig template
+$title = Addons\MKTP_TITLE;
+$subtitle = Addons\MKTP_SUBTITLE;
+// variable to check user login
+$login = false;
+$page = array(
+        'title' => $title,
+        'subtitle' => $subtitle,
+        'login'=> $login
+      );
+// intial twig
+$loader = new Twig_Loader_Filesystem('../views');
+$twig = new Twig_Environment($loader);
+$twig->render('index.twig',$page);
