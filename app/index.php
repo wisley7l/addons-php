cf1d173ced06a3a8b2cb63ad1f2bf235e2b8195b
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-09 12:56:46 -0200

    update index(.php and .html)

--- app/index.php
+++ app/index.php
@@ -6,6 +6,8 @@ $title = Addons\MKTP_TITLE;
 $subtitle = Addons\MKTP_SUBTITLE;
 // variable to check user login
 $login = false;
+// obs: tratar idioma
+$welcome = 'Bem vindo a';
 
 // intial twig
 $loader = new Twig_Loader_Filesystem(Addons\PATH_APP . '/views');
@@ -13,5 +15,8 @@ $twig = new Twig_Environment($loader);
 echo $twig->render('index.twig', array(
   'title' => $title,
   'subtitle' => $subtitle,
-  'login'=> $login
+  'login' => $login,
+  'welcome' => $welcome,
+  'e_com' => 'E-Com Plus',
+  'addons' => 'Adonns'
 ));
