600e430704709242513b483453f00a865a706157
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-09 13:42:07 -0200

    update index(.php)

--- app/index.php
+++ app/index.php
@@ -6,8 +6,17 @@ $title = Addons\MKTP_TITLE;
 $subtitle = Addons\MKTP_SUBTITLE;
 // variable to check user login
 $login = false;
+
 // obs: tratar idioma
-$welcome = 'Bem vindo a';
+switch ($_SERVER['PATH_LANG']) {
+  case 'pt_br':
+    $welcome = 'Bem vindo a ';
+    break;
+
+  default:
+    $welcome = 'Welcome to ';
+    break;
+}
 
 // intial twig
 $loader = new Twig_Loader_Filesystem(Addons\PATH_APP . '/views');
