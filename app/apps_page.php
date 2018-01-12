824a439dd3313a241b324080f3e235d82a255d35
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-12 21:59:19 -0200

    fix apps page

--- app/apps_page.php
+++ app/apps_page.php
@@ -1,8 +1,11 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 $dictionary = get_dictionary();
+$login = true;
 // intial twig and send varibles for template
 $loader = new Twig_Loader_Filesystem(Addons\PATH_APP . '/views');
 $twig = new Twig_Environment($loader);
 echo $twig->render('apps_page.twig', array(
+  'dictionary' => $dictionary,
+  'login' -> $login
 ));
