ff7fc84d7208378cef40f554010f43d071b12cca
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-12 20:25:47 -0200

    mult coin

--- app/index.php
+++ app/index.php
@@ -19,9 +19,11 @@ $all_category = array(
   'c3',
   'c4'
 );
+// get dictionary
+$dictionary = get_dictionary();
 // test apps  // Perform db query to obtain this information
 $apps = array(
-  array('name' => 'APP1', 'name_partner' => 'Partner 1', 'value' => 12.03 ,'star_on' => 3, 'star_off' => 2, 'image' => '../images/items/westeros_m.jpg' ),
+  array('name' => 'APP1', 'name_partner' => 'Partner 1', 'value' => $dictionary['mult_coin'] * 12.03 ,'star_on' => 3, 'star_off' => 2, 'image' => '../images/items/westeros_m.jpg' ),
   array('name' => 'APP2', 'name_partner' => 'Partner 2', 'value' => 15.50 ,'star_on' => 4, 'star_off' => 1, 'image' => '../images/items/westeros_m.jpg' ),
   array('name' => 'APP3', 'name_partner' => 'Partner 2', 'value' => 15.50 ,'star_on' => 0, 'star_off' => 0, 'image' => '../images/items/westeros_m.jpg' )
 );
@@ -31,8 +33,7 @@ $themes = array(
   array('name' => 'THEME2', 'name_partner' => 'Partner 5', 'value' => 15.50 ,'star_on' => 0, 'star_off' => 5, 'image' => '../images/items/westeros_m.jpg' ),
   array('name' => 'THEME3', 'name_partner' => 'Partner 6', 'value' => 15.50 ,'star_on' => 5, 'star_off' => 0, 'image' => '../images/items/westeros_m.jpg' )
 );
-// get dictionary
-$dictionary = get_dictionary();
+
 // intial twig and send varibles for template
 $loader = new Twig_Loader_Filesystem(Addons\PATH_APP . '/views');
 $twig = new Twig_Environment($loader);
