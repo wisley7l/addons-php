932c74cec2145b7839cce7d4545f4c3eeb658a30
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-15 12:10:34 -0200

    fix name files

--- app/apps-page.php
+++ app/apps-page.php
@@ -46,7 +46,7 @@ $apps = array(
 // intial twig and send varibles for template
 $loader = new Twig_Loader_Filesystem(Addons\PATH_APP . '/views');
 $twig = new Twig_Environment($loader);
-echo $twig->render('apps_page.twig', array(
+echo $twig->render('apps-themes-page.twig', array(
   'dictionary' => $dictionary,
   'login' => $login,
   'info_footer' => $info_footer,
