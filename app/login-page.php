d6fd254b819a89a3bdcc53032d7e0d764db12b90
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-16 09:15:48 -0200

    create popup

---
+++ app/login-page.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+// get dictionary
+$dictionary = get_dictionary();
+
+
+
+// intial twig and send varibles for template
+$loader = new Twig_Loader_Filesystem(Addons\PATH_APP . '/views');
+$twig = new Twig_Environment($loader);
+echo $twig->render('login-page.twig', array(
+  'dictionary' => $dictionary,
+));
