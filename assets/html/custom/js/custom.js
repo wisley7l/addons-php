e50365e96cfb3f615ec2ec22c1473332568ea34b
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-16 17:40:02 -0200

    edit form  script md5

--- assets/html/custom/js/custom.js
+++ assets/html/custom/js/custom.js
@@ -1,8 +1,9 @@
-$("form.addons-partner-login").submit(function() {
+$("form.addons-partner-login").submit(function(event) {
   console.log("estou aqui");
   let username = $(this).find("input.addons-login-user").val();
   let password = md5($(this).find("input.addons-login-password").val());
   $("#addons-login-user").val(username);
   $("#addons-login-pass").val(password);
   $("#addons-login").submit();
+  event.preventDefault();
 });
