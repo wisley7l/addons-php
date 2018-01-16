7aaea90d774c91d6b5d333b669b2f48373b91e6f
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-16 17:19:09 -0200

    include scripts js

---
+++ assets/html/custom/js/custom.js
@@ -0,0 +1,7 @@
+$("form.addons-partner-login").submit(function() {
+  let username = $(this).find("input.addons-login-user").val();
+  let password = md5($(this).find("input.addons-login-password").val());
+  $("#addons-login-user").val(username);
+  $("#addons-login-pass").val(password);
+  $("#addons-login").submit();
+});
