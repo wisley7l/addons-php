d3cd4046c51c60f687954faea4b28e5b4374f463
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-16 09:54:42 -0200

    move description config

--- config/config-default.php
+++ config/config-default.php
@@ -24,9 +24,11 @@ const MKTP_TITLE = 'E-Com Plus Addons';
 switch ($_SERVER['PATH_LANG']) {
   case 'pt_br':
     define('Addons\MKTP_SUBTITLE', 'Marketplace de apps e temas para e-commerce');
+    define('Addons\MKTP_DESC_FOOTER','é o melhor marketplace de apps e temas para e-commerce, criado para possibilitar que lojistas que não tem grande capacidade de investimento tenham acesso a um layout personalizado de qualidade pelo melhor preço.');
     break;
 
   default:
     define('Addons\MKTP_SUBTITLE', 'Ecommerce apps and themes marketplace');
+    define('Addons\MKTP_DESC_FOOTER','is the best e-commerce theme and app marketplace designed to enable retailers who do not have large investment capacity to access a customized quality layout at the best price.');
     break;
 }
