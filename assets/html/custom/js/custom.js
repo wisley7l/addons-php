c702efbc864181fc81fb33b2c204a56a64a5ce57
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-16 17:23:49 -0200

    test include scripts js

--- assets/html/custom/js/custom.js
+++ assets/html/custom/js/custom.js
@@ -1,4 +1,5 @@
 $("form.addons-partner-login").submit(function() {
+  console.log("estou aqui");
   let username = $(this).find("input.addons-login-user").val();
   let password = md5($(this).find("input.addons-login-password").val());
   $("#addons-login-user").val(username);
