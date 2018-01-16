531be1a19f9a00ed68760f6b5c818f77307f14af
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-16 15:46:14 -0200

    form login

--- lib/login-validation.php
+++ lib/login-validation.php
@@ -2,8 +2,10 @@
 // check is user, is store or partner and send information to section because the menus are different
 //
 if (!empty($_POST) AND (empty($_POST['username5']) OR empty($_POST['password5']))) {
-      header("Location: index.php"); exit;
+      header("Location: index.php");
+      exit;
   }
 else {
-    header("Location: test-server.php"); exit;
+    header("Location: test-server.php");
+    exit;
 }
