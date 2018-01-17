9f6760cedce463eb9c77908757014947250bbde2
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-17 10:14:50 -0200

    update test erro msg

--- app/scripts/login.php
+++ app/scripts/login.php
@@ -1,5 +1,7 @@
 <?php
 if (!empty($_POST) AND (empty($_POST['user']) OR empty($_POST['pass']))) {
-     header("Location: ../index");
+
+    header("location:javascript:alert(\"Email enviado com Sucesso!\");location.href=\"../index.php\";");
+    // header("Location: ../index");
      exit;
  }
