f53d3003c0b993d2a86dd076a2c23afea0b9adaf
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-17 10:19:51 -0200

    test alert

--- app/scripts/login.php
+++ app/scripts/login.php
@@ -1,7 +1,7 @@
 <?php
 if (!empty($_POST) AND (empty($_POST['user']) OR empty($_POST['pass']))) {
 
-    header("location:javascript:alert(\"Email enviado com Sucesso!\");location.href=\"../index.php\";");
-    // header("Location: ../index");
+    echo "<script>alert('Email enviado com Sucesso!);</script>";
+    header("Location: ../index");
      exit;
  }
