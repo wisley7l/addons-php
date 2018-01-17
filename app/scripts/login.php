2d9ffd6a1f28003ccf29a3763f8d7e8b58195c07
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-17 10:56:13 -0200

    test alert

--- app/scripts/login.php
+++ app/scripts/login.php
@@ -1,6 +1,12 @@
 <?php
-if (!empty($_POST) AND (empty($_POST['user']) OR empty($_POST['pass']))) {
+// check if user and password are null, if null, redirect to index
+if (!empty($_POST) AND (empty($_POST['user']) OR empty($_POST['pass']))){
+  header("Location: ../index");
+  exit;
+}
+// if they are not null, treat variables and query the database
+else {
+  $user = $_POST['user'];
+  $pass = $_POST['pass'];
 
-    header("Location: ../index");
-     exit;
- }
+}
--- assets/html/custom/js/main.js
+++ assets/html/custom/js/main.js
@@ -1,4 +1,5 @@
 $("form.addons-partner-login").submit(function(event) {
+  alert("Test");
   let username = $(this).find("input.addons-login-user").val();
   let password = md5($(this).find("input.addons-login-password").val());
   $("#addons-login-user").val(username);
