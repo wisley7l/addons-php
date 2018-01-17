06083476629bb38e84cd3a2ce2b531f8fba382e2
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-17 15:06:21 -0200

    test in login

--- assets/html/custom/js/main.js
+++ assets/html/custom/js/main.js
@@ -8,7 +8,7 @@ $("form.addons-partner-login").submit(function(event) {
 });
 
 $( document ).ready(function() {
-  if ($(this).find("div.wis2")) {
+  if ($(this).find("div.wis2").length > 0) {
     alert("OK");
   }
 });
