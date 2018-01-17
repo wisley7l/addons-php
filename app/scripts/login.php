b09fbc7352feb7c774d9aec0c6ddf485565924b5
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-17 10:22:39 -0200

    delete test alert

--- app/scripts/login.php
+++ app/scripts/login.php
@@ -1,7 +1,6 @@
 <?php
 if (!empty($_POST) AND (empty($_POST['user']) OR empty($_POST['pass']))) {
 
-    echo "<script>alert('Email enviado com Sucesso!');</script>";
     header("Location: ../index");
      exit;
  }
