b9906af443631b75a7122c70ce69f51de1e7ea03
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-18 12:36:21 -0200

    delete path sessions in scripts

--- main.php
+++ main.php
@@ -51,8 +51,7 @@ if (file_exists(__DIR__ . '/config/config.php')) {
   require __DIR__ . '/config/config-default.php';
 }
 // define path to sessions
-chmod (Addons\PATH_APP.'/app/scripts/sessions', 0777);
-session_save_path(Addons\PATH_APP.'/app/scripts/sessions');
+session_save_path(Addons\PATH_APP.'/app/scripts');
 ini_set('session.gc_probability', 1);
 // load composer packages
 require __DIR__ . '/composer/vendor/autoload.php';
