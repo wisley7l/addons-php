d1026637a4bb35d866bc96a7a7f0fa8f6c057998
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-18 09:40:56 -0200

    test session user_name

--- app/index.php
+++ app/index.php
@@ -14,7 +14,7 @@ if (isset($_GET['SUCESSLOGIN'])) {
 session_start();
 
 if (isset($_SESSION)) {
-  print '<div class=""> ok '. $_SESSION['login'] . '</div>';
+  print '<div class=""> ok '. $_SESSION['user_name'] . '</div>';
 }
 
 // TODO: parse twig template
