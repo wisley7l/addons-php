3703206859393fd03fda282dae5b4bb57aac2f23
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-22 14:13:00 -0200

    test reputation

--- app/profile-page.php
+++ app/profile-page.php
@@ -62,7 +62,7 @@ $info_author = array(
   'path_image' => '../images/avatars/avatar_09.jpg',
   'number_apps_themes' => 3,
   'number_badges' => 1,
-  'stars' => 3,
+  'stars' => 1,
   'evaluations' => 5
 );
 // test apps  // Perform db query to obtain this information limit 3
