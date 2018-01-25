34efaf14a43d8fedc0823651a8903506ddfe3c9e
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-25 12:25:35 -0200

    test treat click

--- assets/html/custom/js/main.js
+++ assets/html/custom/js/main.js
@@ -102,7 +102,7 @@ $("#board-profile-page").click(function(){
 });
 $(document).ready(function(){
   let l = window.location.pathname;
-  l.replace('/pt_br/','');
-  l.replace('/en_us/','');
+  l.replace(/pt_br/g,'');
+  l.replace(/en_us/g,'');
   console.log(l);
 });
