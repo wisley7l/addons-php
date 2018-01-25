2e1e6b6764939484769fad9583e92d64c6d1db8f
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-25 10:04:38 -0200

    profile page

--- app/profile-page.php
+++ app/profile-page.php
@@ -93,15 +93,15 @@ array_push($apps, $item2);
 // query apps in db
 $item = array(
   'id_app' => 1000,
-  'name' => 'APP1',
-  'id_partner' => 2,
-  'name_partner' => 'Partner 1',
-  'value' => 12.03 / $dictionary['mult_coin'],
+  'name' => 'THEME2',
+  'id_partner' => 1,
+  'name_partner' => 'Partner 2',
+  'value' => 20.03 / $dictionary['mult_coin'],
   'star_on' => 3,
   'star_off' => 2,
   'image' => '../images/items/westeros_m.jpg',
-  'image_partner' => 'http://2.bp.blogspot.com/-7tydAWF_j7o/VbI736K_lTI/AAAAAAAADs4/vM0V-5nFTKw/s1600/boteco%2Bde%2Boa%2B%25285%2529.jpg',
-  'is_app'=> true
+  'image_partner' => 'http://bluebus-wp.s3.amazonaws.com/wp-content/uploads/2014/04/Mike-Mitchell-Marvel-Portraits-Time-mitchell_spiderman_press.jpg',
+  'is_app'=> false
 );
 $themes = array();
 // add element in array
