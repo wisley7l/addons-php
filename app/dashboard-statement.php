32e988ea140539bf2fd558e807e677a8482c90df
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-26 10:43:44 -0200

    create dashboard-statment

--- app/dashboard-statement.php
+++ app/dashboard-statement.php
@@ -2,7 +2,7 @@
 // intial twig and send varibles for template
 $loader = new Twig_Loader_Filesystem(Addons\PATH_APP . '/views');
 $twig = new Twig_Environment($loader);
-echo $twig->render('dashboard-settings.twig', array(
+echo $twig->render('dashboard-statement.twig', array(
   'dictionary' => $dictionary,
   'login' => $login,
   'implemented' => false,
