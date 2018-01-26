7639dc84f954f1a0ba3a7a9e04c54c38e37f77e3
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-26 10:46:11 -0200

    create dashboard-statment

--- app/dashboard-statement.php
+++ app/dashboard-statement.php
@@ -1,4 +1,34 @@
-<?php
+dashboard-statement<?php
+header('Content-Type: text/html; charset=utf-8');
+// get dictionary
+$dictionary = get_dictionary();
+// variable to check the user login, because some options are only allowed for online users
+$login = false;
+
+//(init) * Required on all pages *
+// close writing session, if it exists and intal session
+session_write_close();
+session_start();
+// if the session exists
+if (isset($_SESSION)) {
+  //modify the value of the login variable, by the value saved in the session
+  //var_dump($_SESSION);
+  $login = $_SESSION['login'];
+  // set values for user, with the values saved in the session
+  // array used to set user panel parameters
+  $user_login = getUserLogin($dictionary);
+  // query the user in db for more information to update
+  // ex: about user, website, email
+}
+// check if logout attempt
+if (isset($_GET['logout'])){
+  // if attempt is true, destroy session values and redirect to index page
+  session_destroy();
+  // obs. check redirection on all pages
+  header("Location: index");
+}
+//(end) * Required on all pages *
+
 // intial twig and send varibles for template
 $loader = new Twig_Loader_Filesystem(Addons\PATH_APP . '/views');
 $twig = new Twig_Environment($loader);
