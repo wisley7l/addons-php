97944c40916c1ea2226387a872c7d2e1740ba855
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-30 15:21:00 -0200

    test selected app theme

--- app/categories/product-sourcing.php
+++ app/categories/product-sourcing.php
@@ -97,7 +97,7 @@ $filter_segment = array(
 // intial twig and send varibles for template
 $loader = new Twig_Loader_Filesystem(Addons\PATH_APP . '/views');
 $twig = new Twig_Environment($loader);
-echo $twig->render('apps-themes-page.twig', array(
+echo $twig->render('search-category.twig', array(
   'dictionary' => $dictionary,
   'login' => $login,
   'info_footer' => $info_footer,
