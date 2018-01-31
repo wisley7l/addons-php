655c49a932b8c5073bc4702b9b770b0520595678
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-01-31 09:29:21 -0200

    create item page

--- app/item-page.php
+++ app/item-page.php
@@ -7,3 +7,23 @@ if (isset($_GET['id']) AND isset($_GET['app'])){
 else {
   echo "error";
 }
+
+
+
+
+// intial twig and send varibles for template
+$loader = new Twig_Loader_Filesystem(Addons\PATH_APP . '/views');
+$twig = new Twig_Environment($loader);
+echo $twig->render('item-page.twig', array(
+  'dictionary' => $dictionary,
+  'login' => $login,
+  'info_footer' => $info_footer,
+  'info_page' => $info_page,
+  'segment' => $filter_segment,
+  'all_category' => $all_category,
+  'filter' => $filter,
+  // test apps
+  'apps_themes' => $apps,
+  'user' => $user_login,
+  'search_id' => $id_category
+));
