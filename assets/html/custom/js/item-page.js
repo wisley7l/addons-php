d7c78154c45b5ad6631c39f83ebbe5330d5b759b
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-02 15:47:30 -0200

    edit itempage

--- assets/html/custom/js/item-page.js
+++ assets/html/custom/js/item-page.js
@@ -1,33 +1,36 @@
 (function($,event) {
-  var $div = $('#template');
+  var $div_button = $('#template');
+  let $div_item = $('div#addons-items');
   let page = window.location.hash;
   //let profile_page = page.indexOf("profile-page");
   var res = page.split("#template");
   console.log(parseInt(res[1]));
+  let id = parseInt(res[1]);
+  $div_item.find('div#image-' + id).attr('style', '' );
 
-  $div.on( 'click', select );
+  $div_button.on( 'click', select );
 
   function select(event) {
     let target = $(event.target);
     //console.log(target[0].id);
     id_template = target[0].id;
-    let div = 'div#addons-items';
 
-    let view = $(div).find('div#image-' + id_template);
+
+    let view = $div_item.find('div#image-' + id_template);
     let button =$div.find('a#' + id_template);
     //console.log('Template: ');
     //console.log(button);
     //$(div).find('div#image-' + id_template).attr('style', '' );
 
-    $div.each(function() {
+    $div_button.each(function() {
       var $this = $(this),
         buttonID = $this.find('a');
       //  console.log(buttonID);
       for (var i = 0; i < buttonID.length; i++) {
         if (buttonID[i]== button[0]) {
-          $(div).find('div#image-' + buttonID[i].id).attr('style', '' );
+          $div_item.find('div#image-' + buttonID[i].id).attr('style', '' );
         }else {
-          $(div).find('div#image-' + buttonID[i].id).attr('style', 'display:none;' );
+          $div_item.find('div#image-' + buttonID[i].id).attr('style', 'display:none;' );
         }
 
       }
