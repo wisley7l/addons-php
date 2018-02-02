cc9dfee920da67382a6e7bb91ffd110d7a9e788c
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-02 13:03:42 -0200

    edit itempage

--- assets/html/custom/js/item-page.js
+++ assets/html/custom/js/item-page.js
@@ -2,10 +2,12 @@
   var $div = $('#template');
   console.log('ok');
   console.log($div);
-  
+
   $div.on( 'click', select );
 	function select() {
-      console.log('click');
+    let $this = $(this),
+			selectedCheckboxID = $this.prop('id')
+      console.log(selectedCheckboxID);
   }
 
 })(jQuery);
