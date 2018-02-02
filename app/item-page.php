a65bb5cf8921446f19340d64f47d1b3fa078da86
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-02 10:28:10 -0200

    edit itempage

--- app/item-page.php
+++ app/item-page.php
@@ -62,7 +62,7 @@ if (isset($_GET['id']) AND isset($_GET['app'])){
   if (isset($_GET['template'])) {
     foreach ( $array_images as $key ) {
       foreach ($key as $i) {
-        echo $i;
+        echo $i['id'];
       }
     }
   } else {
