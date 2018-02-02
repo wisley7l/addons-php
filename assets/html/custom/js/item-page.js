adc670ceeeddfebaa49dc09274b5da7fda9ebb18
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-02 14:19:09 -0200

    edit itempage

--- assets/html/custom/js/item-page.js
+++ assets/html/custom/js/item-page.js
@@ -8,9 +8,11 @@
     //console.log(target[0].id);
     id_template = target[0].id;
     let div = 'div#addons-items';
+
     let view = $(div).find('div#image-' + id_template);
+    let button =$(div).find('a#' + id_template);
     //console.log('Template: ');
-    console.log(view);
+    console.log(button);
     $(div).find('div#image-' + id_template).attr('style', '' );
 
     $div.each(function() {
