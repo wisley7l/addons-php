99a818662cf6672bc3ae64293a1cbff92102b63e
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-05 21:58:53 -0200

    edit item page

--- assets/html/custom/js/item-page.js
+++ assets/html/custom/js/item-page.js
@@ -52,11 +52,11 @@ $('#buy-item').click(function(){
     //If there is more than one template, capture template id.
     $div_item.each(function(){
       let views = $(this).find('div');
+      console.log(views);
       for (var i = 0; i < views.length; i++) {
         if (views[i].style.display == 'block') {
           console.log(views[i]);
         }
-        console.log(views[i]);
       }
 
     })
