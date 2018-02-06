e33ecc57a80cdcf0f380c036d8aedc21e7d515b1
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-06 16:02:28 -0200

    edit dashboard uploaditem

--- assets/html/custom/js/uploaditem.js
+++ assets/html/custom/js/uploaditem.js
@@ -1,3 +1,4 @@
+let num_category = 1;
 $("#send_review_item").click(function(event){
   let $form = $('form#upload_form'),
       category = $form.find('select#category').val();
@@ -8,5 +9,10 @@ event.preventDefault();
 });
 
 $("#add_category").click(function(event){
-  console.log('click');
+  num_category +=;
+  console.log(num_category);
+
+
+
+  event.preventDefault();
 })
