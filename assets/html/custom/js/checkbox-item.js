229ea5daeac29b93b82c1e0b65f645ead4bcc38f
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-06 10:10:57 -0200

    check box js

--- assets/html/custom/js/checkbox-item.js
+++ assets/html/custom/js/checkbox-item.js
@@ -80,7 +80,8 @@ $("#buy-item").click(function(event){
       let divs = $(this).find('div');
       for (var i = 0; i < divs.length; i++) {
         if (divs[i].style.display == 'block' && divs[i].id.indexOf("image-") != -1) {
-          console.log(divs[i].split("image-")[1]);
+          let id_template = divs[i].id.split("image-")
+          console.log(id_template);
         }
       }
     })
