019ae7901eec7b98d6d4556508eacd836dc237ba
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-06 15:53:07 -0200

    edit dashboard uploaditem

--- assets/html/custom/js/uploaditem.js
+++ assets/html/custom/js/uploaditem.js
@@ -1,7 +1,7 @@
 $("form#upload_form").submit(function(event){
   let $form = $('form#upload_form'),
-      category = $form.find('select#category');
-      console.log(category[0].value);
+      category = $form.find('select#category').val();
+      console.log(category);
 
 
 event.preventDefault();
