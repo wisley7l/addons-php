888b77e4e0387ed600a000976abf530422c69415
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-06 13:28:16 -0200

    edit macro

--- app/scripts/buy.php
+++ app/scripts/buy.php
@@ -1,3 +1,14 @@
 <?php
-var_dump($_POST);
-echo "Test";
+//var_dump($_POST);
+//echo "Test";
+/*
+  treat variables for purchase
+
+  check login and if you are a shopkeeper.
+  if not logged in, redirect to login screen (create login screen obs: moblie)
+  if it is app create query for plans
+  if it's thema create want to buy_theme
+*/
+if ($_POST['is_app']) {
+  echo "app";
+}
