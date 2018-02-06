df60f3098eec65696b5112a04f4f908d11f9fa2e
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-06 10:05:26 -0200

    check box js

--- assets/html/custom/js/checkbox-item.js
+++ assets/html/custom/js/checkbox-item.js
@@ -50,32 +50,46 @@
 
 // buy item click buy
 $("#buy-item").click(function(event){
-  let div = 'div#item-page',
-      input = $(div).find('input.checkbox-plan');
-  for (var i = 0; i < input.length; i++) {
-    if (input[i].checked == true) {
-      //console.log(input[i]);
-      id_license = input[i].id;
-      console.log(input[i]);
-    }
-  }
-  let value = $(div).find('input#i-'+id_license).val();
-  console.log(value);
   /*  capture item id
-      License ID
-      value
+      License ID ok
+      value ok
       shopkeeper id
       if it's app or theme
   */
+  let div = 'div#item-page',
+      input = $(div).find('input.checkbox-plan'),
+      div_id = 'div#addons-buy',
+      $div_item = $('div#addons-items'),
+      id_app = $(div_id).find('input#id-item').val(), // capture app id
+      is_app = $(div_id).find('input#item-is-app').val(); // capture variable that checks whether it's app
+  for (var i = 0; i < input.length; i++) {
+    if (input[i].checked == true) { // check which plan is selected
+      id_license = input[i].id; // (id plan is string) this is the id of the selected plane
+    }
+  }
+  let value = parseFloat($(div).find('input#i-'+ id_license).val()); // this is the value of the selected plan
+  // if is_app = 1 app
+  // else theme
+  if (is_app == 1) { // treat app purchase
+    //
 
+  }
+  else { // treat theme purchase
+    //If there is more than one template, capture template id.
+    $div_item.each(function(){
+      let divs = $(this).find('div');
+      for (var i = 0; i < divs.length; i++) {
+        if (divs[i].style.display == 'block' && divs[i].id.indexOf("image-") != -1) {
+          console.log(divs[i]);
+        }
+      }
+    })
+  }
 });
 /*
 // treat click buy now
 $('#buy-item').click(function(){
-  let div_id = 'div#addons-buy',
-      $div_item = $('div#addons-items'),
-      id_app = $(div_id).find('input#id-item').val(),
-      is_app = $(div_id).find('input#item-is-app').val(),
+  let
       var $checkbox = $('.linked-check');
       let id = $checkbox.prop('for');
       let price1 = parseFloat($checkbox.find('input#'+id).val());
