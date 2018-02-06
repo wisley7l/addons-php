731e2cff65a14b6e1a502d9de3d698c38a51c140
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-06 16:28:28 -0200

    edit dashboard uploaditem

--- assets/html/custom/js/uploaditem.js
+++ assets/html/custom/js/uploaditem.js
@@ -1,15 +1,21 @@
 var num = 1;
 $("form#upload_form").submit(function(event){
   let $form = $('form#upload_form'),
-      category = $form.find('select#category').val();
-      console.log(category);
+      categories = $form.find('select#category').val(), // not null
+      name_app = $form.find('input#item_name').val(), // not null
+      num_version = $form.find('input#item_numversion').val(),
+      description = $form.find('input#item_description').val(),// not null
+      script_url = $form.find('input#item_scripturl').val(),
+      github = $form.find('input#item_github').val(),
+      website = $form.find('input#item_website').val(),
+      video = $form.find('input#item_linkvideo').val();
 
-
-event.preventDefault();
+  console.log(name_app);
+  event.preventDefault();
 });
 
 $("#add_category").click(function(event){
-  num += 1; 
-  console.log(num);
+  num += 1;
+  //console.log(num);
   event.preventDefault();
 })
