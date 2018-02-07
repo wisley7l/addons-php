c399146954155505f242332cff8e6a96e5d37087
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-07 10:53:38 -0200

    edit dashboard upload item

--- assets/html/custom/js/uploaditem.js
+++ assets/html/custom/js/uploaditem.js
@@ -40,3 +40,48 @@ $("#add_category").click(function(event){
   //console.log(num);
   event.preventDefault();
 })
+
+
+(function($) {
+	var $checkbox = $('.label-check');
+  console.log($checkbox);
+
+	/*$checkbox.on( 'click', deselectLinked );
+
+	function deselectLinked() {
+		var $this = $(this),
+			selectedCheckboxID = $this.prop('for'),
+			selectedCheckboxStatus = $("#"+selectedCheckboxID).prop('checked');
+			showDescription(selectedCheckboxID);
+
+			$checkbox.each(function() {
+				var $this = $(this),
+					checkboxID = $this.prop('for'),
+					checkboxStatus = $("#"+checkboxID).prop('checked');
+
+				if( checkboxID != selectedCheckboxID ) {
+					deselect($("#"+checkboxID))
+					hideDescription(checkboxID);
+					changePrice("<span>$</span>28.00");
+				} else {
+					changePrice("<span>$</span>56.00");
+				}
+			});
+	}
+
+	function deselect(checkbox) {
+		checkbox.prop('checked', false);
+	}
+
+	function showDescription(container) {
+		$(".license-text[data-license='"+container+"']").slideDown();
+	}
+
+	function hideDescription(container) {
+		$(".license-text[data-license='"+container+"']").slideUp();
+	}
+
+	function changePrice(price) {
+		$('.sidebar-item .price.large').html(price);
+	}*/
+})(jQuery);
