eda53879042b9a96365dd478e50fd25d704a6504
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-07 16:04:19 -0200

    update uploaditem

--- app/scripts/uploaditem.php
+++ app/scripts/uploaditem.php
@@ -1,2 +1,32 @@
 <?php
 var_dump($_POST);
+
+if(empty($_POST)) { // not exist post
+  header("Location: ../");
+} elseif (!empty($_POST) and (empty($_POST['is_app']) ) ) {
+  // is app not defined
+  header("Location: ../");
+} elseif (!empty($_POST) and (!empty($_POST['is_app']) ) ) {
+  // is app defined
+  $is_app  = (int) $_POST['is_app'];
+  /*
+  is_app == 1 create app, == 0 create theme
+  name // name
+  category // relationship category and item
+  numversion // app and theme
+  description // app and theme
+  scripturl // app
+  github // app
+  website // app
+  linkvideo // app and theme
+  linkdoc // theme
+  */
+  if ($is_app == 1 ) {
+    echo "Create app";
+  } elseif ($is_app == 0 ) {
+    echo "create theme";
+  } else {
+    echo "Error";
+  }
+
+}
