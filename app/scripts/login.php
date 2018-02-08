e139e0bf7d23af0afeeed7898692b2351cba050b
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-08 10:37:32 -0200

    create erro page

--- app/scripts/login.php
+++ app/scripts/login.php
@@ -2,7 +2,7 @@
 // Occurs after clicking the login button now
 // check if user and password are null, if null, redirect to index
 if (!empty($_POST) AND (empty($_POST['user']) OR empty($_POST['pass']))){
-  header("Location: ../?EROORLOGIN");
+  header("Location: error-page");
   exit;
 }
 // if they are not null, treat variables and query the database
