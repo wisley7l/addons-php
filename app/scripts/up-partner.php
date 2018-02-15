897a6344ac8e6e1b1aaf1010d34dc60c52ab6efc
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-15 17:37:33 -0200

    edit dashboard

--- app/scripts/up-partner.php
+++ app/scripts/up-partner.php
@@ -24,14 +24,17 @@ if ( isset( $_FILES[ 'image' ][ 'name' ] ) && $_FILES[ 'image' ][ 'error' ] == 0
     // Somente imagens, .jpg;.jpeg;.gif;.png
     // Aqui eu enfileiro as extensões permitidas e separo por ';'
     // Isso serve apenas para eu poder pesquisar dentro desta String
-    /*if ( strstr ( '.jpg;.jpeg;.gif;.png', $extensao ) ) {
+    //*
+    if ( strstr ( '.jpg;.jpeg;.gif;.png', $extensao ) ) {
         // Cria um nome único para esta imagem
         // Evita que duplique as imagens no servidor.
         // Evita nomes com acentos, espaços e caracteres não alfanuméricos
         $novoNome = uniqid ( time () ) . '.' . $extensao;
 
         // Concatena a pasta com o nome
-        $destino = 'imagens / ' . $novoNome;
+        $destino = Addons\PATH_DATA . 'images/profile/ ' . $novoNome;
+        echo PHP_EOL;
+        echo $destino;
 
         // tenta mover o arquivo para o destino
         if ( @move_uploaded_file ( $arquivo_tmp, $destino ) ) {
@@ -41,9 +44,10 @@ if ( isset( $_FILES[ 'image' ][ 'name' ] ) && $_FILES[ 'image' ][ 'error' ] == 0
         else
             echo 'Erro ao salvar o arquivo. Aparentemente você não tem permissão de escrita.<br />';
     }
-    
+
     else
-        echo 'Você poderá enviar apenas arquivos "*.jpg;*.jpeg;*.gif;*.png"<br />'; */
+        echo 'Você poderá enviar apenas arquivos "*.jpg;*.jpeg;*.gif;*.png"<br />';
+        //*/
 }
 else
     echo 'Você não enviou nenhum arquivo!';
