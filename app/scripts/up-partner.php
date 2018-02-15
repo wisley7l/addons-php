5d8283f100f8f6763c11e8f340cc83f9cc48d7e4
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-15 18:19:31 -0200

    edit dashboard

--- app/scripts/up-partner.php
+++ app/scripts/up-partner.php
@@ -41,7 +41,7 @@ if ( isset( $_FILES[ 'image' ][ 'name' ] ) && $_FILES[ 'image' ][ 'error' ] == 0
         echo PHP_EOL;
         echo $destino;
         echo PHP_EOL;
-        echo substr(sprintf('%o', fileperms('/tmp')), -4);
+        echo substr(sprintf('%o', fileperms('/home/wisley')), -4);
         echo PHP_EOL;
         //*
         // tenta mover o arquivo para o destino
