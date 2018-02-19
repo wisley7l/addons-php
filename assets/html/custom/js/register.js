fec1e385d746295b80c85b416bb21a00f5cc0cc7
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-19 17:54:18 -0300

    edit register page

--- assets/html/custom/js/register.js
+++ assets/html/custom/js/register.js
@@ -16,10 +16,10 @@ $('form.addons-partner-create-pass').submit(function(event){
 
 //
 $('form#addons-form-password').submit(function(event){
-  let $div = $("div.form-box-item");
-  let pass = md5($div.find("input.addons-pass").val()),
+  let $div = $("div.form-box-item"),
+      pass = md5($div.find("input.addons-pass").val()),
       rp_pass = md5($div.find("input.addonsrp-pass").val()),
-      empty = md5('');
+      empty = $div.find("input.addons-pass").val();
   if(pass != rp_pass || pass == ''){
     //If the confirmation password and password are different, activate "span"
     $div.find("span.addons-pass").css( "color", "red");
