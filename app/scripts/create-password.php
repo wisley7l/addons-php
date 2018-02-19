3aa7e3c538af021e51f7533101bc8ceb5f215c2f
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-19 16:03:18 -0300

    edit register page

--- app/scripts/create-password.php
+++ app/scripts/create-password.php
@@ -2,6 +2,52 @@
 if (!empty($_POST['email'])) {
   $email = $_POST['email'];
   echo $email;
+  // get in API
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, 'https://e-com.plus/api/v1/partners.json?email='. $email);
+  // prevent execution timeout
+  curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json'
+  ));
+  $output = curl_exec($ch);
+  // all done, close cURL
+  curl_close($ch);
+
+  $json = json_decode($response_body, true);
+  // count($json['result']) > 0
+  // $json['result'][0]['id']
+  if (count($json['result']) > 0) {
+    $id_partner = (int) $json['result'][0]['id'];
+    // if exists partner
+    // OBS :
+    // query check if it already exists in the DB
+    // if there is a return, the user is already registered
+    //otherwise request password
+    //*
+    // create connection to the database
+    $conn = mysqli_connect(Addon\MYSQL_HOST, Addon\MYSQL_USER, Addons\MYSQL_PASS, Addon\MYSQL_DB);
+    // check connection
+    if (mysqli_connect_errno()) {
+      echo 'Connection failed: ';
+      echo mysqli_connect_error();
+      echo PHP_EOL;
+      exit();
+    }
+    // query search app and theme for index page
+    $query = "SELECT `p.id` FROM `partners p`
+      WHERE (`p.id` = $id_partner) LIMIT 1";
+
+    if (mysqli_query(  $conn, $query )) {
+      // message error
+    }else {
+      // request password
+    }
+    //*/
+  }else {
+    // unregistered partner
+  }
 
 }else {
   echo "error";
