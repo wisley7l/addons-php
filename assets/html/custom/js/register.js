8cf512cc86a0ef648c2fe1a5d95996dff5a334ae
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-19 14:22:33 -0300

    edit register page

--- assets/html/custom/js/register.js
+++ assets/html/custom/js/register.js
@@ -1,6 +1,12 @@
-(function($,event) {
+(function(event) {
 $('#bt-create-pass').click(function() {
+  create();
   alert( "Handler for .click() called." );
 });
 
 })(jQuery);
+
+function create() {
+  //create-password
+  $('#create-password').attr('class', 'form-popup mfp-hide')
+}
