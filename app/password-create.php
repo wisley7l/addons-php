ad0fb344144098c3cfe44c329d82f297d9a5cd8e
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-19 16:34:41 -0300

    edit register page

--- app/password-create.php
+++ app/password-create.php
@@ -1,2 +1,11 @@
 <?php
 echo "page create password";
+if (empty([$_GET])) {
+  # error redirect index
+  header("Location: index");
+  exit;
+}elseif (!empty($_GET['id'])) {
+  $id = (int)$_GET['id'];
+  echo PHP_EOL;
+  echo "$id";
+}
