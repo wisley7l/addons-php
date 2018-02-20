f4f6bf2e105ed80dd2c819a72abea2b703bb1658
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-20 14:53:44 -0300

    edit upload partner

--- app/scripts/up-partner.php
+++ app/scripts/up-partner.php
@@ -16,8 +16,8 @@ if (empty($_POST)) {
   if (!empty($_POST['pass'])) {
     $pass_hash = $_POST['pass'];
     echo $pass_hash;
-    $pass_hash = password_hash($pass_hash);
-    echo $pass_hash;
+    $pass = password_hash($pass_hash);
+    echo $pass;
     // query without changing password
      //$query = "UPDATE `partners` SET `password_hash` = $pass_hash  WHERE `id`= $id";
   }else {
