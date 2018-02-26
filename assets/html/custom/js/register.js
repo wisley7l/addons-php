e356fb9b8c2159e10f5b29ec76c328cd4e9fcea0
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-26 12:31:57 -0300

    edit register

--- assets/html/custom/js/register.js
+++ assets/html/custom/js/register.js
@@ -8,7 +8,7 @@ $('form.addons-partner-create-pass').submit(function(event){
   let $form = $(this),
     email = $form.find('#email_register').val();
     console.log(email.length);
-    if (email.length > 0 || email != 'undefined') {
+    if (email.length > 0 && email != 'undefined') {
       console.log('aqui');
       $("#addons-email-user").val(email);
       //$("#addons-create-pass").submit();
