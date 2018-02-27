a5d3af19d17d5889686f274a178c2109fabc23f2
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-27 14:16:19 -0300

    edit create pass

--- assets/html/custom/js/uploaditem.js
+++ assets/html/custom/js/uploaditem.js
@@ -31,8 +31,8 @@ $("form#upload_form").submit(function(event){
       plans_str = '{"total":'+ num_plan + ',"plans": [';
       for (var i = 1; i <= num_plan  ; i++) {
         plans_str += '{"id":' + i + ',"name":"' + $('input#name_plan-'+i).val()
-        + '","value":' + parseFloat($('input#item_value-'+i).val())
-        + ',"desc":"' +  $('input#desc_plan-'+i).val() +'"}';
+        + '","value":"' + $('input#item_value-'+i).val()
+        + '","desc":"' +  $('input#desc_plan-'+i).val() +'"}';
 
         if (i != num_plan) {
           plans_str += ",";
