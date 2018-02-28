a02b42943ddc274958d7801cecec92cd6e693388
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-02-28 16:22:36 -0300

    edit item page

--- app/item-page.php
+++ app/item-page.php
@@ -33,7 +33,7 @@ if (isset($_GET['id']) AND isset($_GET['app'])){
 
     $app_info = array('id' => $id_app,
       'name' => 'APP',
-      'description' => 'kljdasklkdakdasslasdlsadljaksdasdalkkdasdjakldklasklasffkasfsaklçaskldaskhfajskasdfhasdjkdsaasdfjkjdsfld, \n continua teste',
+      'description' => 'kljdasklkdakdasslasdlsadljaksdasdalkkdasdjakldklasklasffkasfsaklçaskldaskhfajskasdfhasdjkdsaasdfjkjdsfld, <p> continua teste </p>',
       'json' => 'treat json'
     );
 
