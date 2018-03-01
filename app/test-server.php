f1df41573805b1d0af7a0d15e7beecfb400b1b6f
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-03-01 18:31:12 -0300

    test converter number

--- app/test-server.php
+++ app/test-server.php
@@ -32,7 +32,7 @@
 // exit;
 $number = 1000;
 $string = strval ($number);
-$eh_aqui = 1;
+$eh_aqui = strlen ( $string ) - 2;
 // //$plan_basic = number_format($number, 2, '', '');
 // //echo $plan_basic;
 $antes = substr($string, 0, $eh_aqui);
