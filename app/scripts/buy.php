384d73c98a33e3c98bec4f23091eb621311f8618
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-03-02 09:05:28 -0300

    edit bay page

--- app/scripts/buy.php
+++ app/scripts/buy.php
@@ -1,4 +1,25 @@
 <?php
+$dictionary = get_dictionary();
+$login = false;
+
+//(init) * Required on all pages *
+// close writing session, if it exists and intal session
+session_write_close();
+session_start();
+// if the session exists
+if (isset($_SESSION)) {
+  //modify the value of the login variable, by the value saved in the session
+  //var_dump($_SESSION);
+  $login = $_SESSION['login'];
+  // set values for user, with the values saved in the session
+  // array used to set user panel parameters
+  $user_login = getUserLogin($dictionary);
+}
+if ($login == false || $_SESSION['login'] == false) { // if not connected
+  //header("Location: ../#ERRORLOGIN");
+  exit;
+}
+
 var_dump($_POST);
 //echo "Test";
 /*
