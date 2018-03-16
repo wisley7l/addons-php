3401664e54f51407a6c1e700dafd884f41370ed4
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-03-16 20:47:13 -0300

    test query index

--- lib/querys_functions.php
+++ lib/querys_functions.php
@@ -21,7 +21,6 @@ function get_app_theme($id_app, $id_partner, $name_app, $image_app, $value_app,
       'is_app'=> $app // if app is equal 1
     );
 }
-/*
 // function to handle partner search and create view
 function getInfoUser($id,$member_since,$path_image,$profile_json)
 {
@@ -40,10 +39,6 @@ function getInfoUser($id,$member_since,$path_image,$profile_json)
     //'stars' => 1, // not implemented
     //'evaluations' => 5 // not implemented
   );*/
-//}
-function FunctionName()
-{
-  # code...
 }
 
 // QUERYS
