a4d60d4e556602450753527702695ad423059af7
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-03-15 21:31:00 -0300

    edit init error create tables

--- init.php
+++ init.php
@@ -37,21 +37,6 @@ if (mysqli_connect_errno()) {
 if (mysqli_query($conn, 'CREATE DATABASE IF NOT EXISTS ' . Addons\MYSQL_DB) === true) {
   echo 'MySQL database created';
   echo PHP_EOL;
-} else {
-  echo 'Failed to create database';
-  echo PHP_EOL;
-  handle_msyql_error($conn);
-}
-
-// update connection to the database
-$conn = mysqli_connect(Addons\MYSQL_HOST, Addons\MYSQL_USER, Addons\MYSQL_PASS, Addons\MYSQL_DB );
-// check connection
-if (mysqli_connect_errno()) {
-  echo 'Connection failed: ';
-  echo mysqli_connect_error();
-  echo PHP_EOL;
-  exit();
-}
 
   // create tables
   // read tables.sql file
@@ -85,3 +70,8 @@ if (mysqli_connect_errno()) {
       handle_mysql_error($conn);
     }
   }
+} else {
+  echo 'Failed to create database';
+  echo PHP_EOL;
+  handle_msyql_error($conn);
+}
