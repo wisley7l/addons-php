9c971786762f0d37b6cf35732f8bfc172986f5b0
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-03-16 20:23:08 -0300

    test query index

--- main.php
+++ main.php
@@ -59,7 +59,7 @@ require __DIR__ . '/lib/dictionary.php';
 // load categories
 require __DIR__ . '/lib/categories.php';
 //load test query php
-// require __DIR__ . '/lib/test-user.php';
+require __DIR__ . '/lib/test-user.php';
 //
 // require __DIR__ . '/lib/query_functions.php';
 // app script
