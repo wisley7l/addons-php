d3e0917dc0b990c46ac65ffcc41dccc9fb7e379e
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-03-17 10:25:43 -0300

    test querys

--- lib/querys_functions.php
+++ lib/querys_functions.php
@@ -168,7 +168,7 @@ function search_apps_all_name($limit,$search)
   $query = "SELECT a.id, a.partner_id,a.title, a.thumbnail,
     a.value_plan_basic,p.id AS p_id, p.path_image
     FROM apps a, partners p
-    WHERE (a.partner_id = p.id AND a.title = $name)
+    WHERE (a.partner_id = p.id AND a.title = '$name')
     ORDER BY a.title
     LIMIT $number ";
 
