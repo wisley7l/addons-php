da2d371b553682d3cd63bb417c36109a0e4857ea
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-03-17 09:29:16 -0300

    test querys

--- app/index.php
+++ app/index.php
@@ -88,7 +88,7 @@ else if (isset($_GET['term']) ){
   // add element in array
   //array_push($themes, $item);
   //OBS: when you are ready to enable these functions below.
-  //$apps = search_all_apps(24); // return a maximum of 25 apps in the search
+  $apps = search_all_apps(24); // return a maximum of 25 apps in the search
   //$themes = search_all_themes(24); // return a maximum of 25 themes in the search
 }
 
