9c825501e1992c9887c7faf5213cb02b13ddfa4e
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-03-25 19:14:50 -0300

    edit upload item

--- app/scripts/uploaditem.php
+++ app/scripts/uploaditem.php
@@ -124,6 +124,8 @@ if(empty($_POST)) { // not exist post
      $plan_basic = $plan['plans'][0]['value'];
 
      for ($i=0; $i < (int) $plans['total_plans'] ; $i++) {
+       echo $plans['plans'][$i]['value'];
+       echo PHP_EOL;
        if ($plan_basic > $plans['plans'][$i]['value']) {
          $plan_basic = $plans['plans'][$i]['value'];
        }
