b8e2761fab0548eb98b024d6420e06c89bede8a9
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-04-23 18:25:17 -0300

    upload item

--- app/scripts/login-store.php
+++ app/scripts/login-store.php
@@ -5,4 +5,23 @@
 //session
 
 //createSession($id,$email,$name,$credits,$image,$is_store)
-createSession(1,'NO-Email','SHOPMAN-TEST',0,NULL,1);
+$conn = connect_db();
+
+$query = "SELECT id, credits FROM store  WHERE id = $id LIMIT 1;";
+
+
+if ($result = mysqli_query(  $conn, $query )) {
+  // fetch associative array
+  while ($row = mysqli_fetch_assoc($result)) {
+    $id = $row['id'];
+    $credit = $row['credits'];
+  }
+  // free result set
+  mysqli_free_result($result);
+}
+
+  if ($id == NULL OR $id == '' ) {
+    //create user
+  }else {
+    //createSession(1,'NO-Email','SHOPMAN-TEST',0,NULL,1);    
+  }
