82519d37fa48bd2c9e3699346b499c0ce3a58a9e
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-05-04 21:54:46 -0300

    upload item

--- app/scripts/buy.php
+++ app/scripts/buy.php
@@ -38,6 +38,7 @@ $id_plan = (int) $_POST['id_plan'];
 if (empty($_POST)) {
   // var_dump($_GET); // get (page car)
 echo "sim";
+$conn = $GLOBALS['conn']; // get varible global conn
   $query =  "SELECT id FROM buy_themes WHERE (theme_id = $id_app AND
     store_id = $id_store AND template_id = $id_template AND payment_status = 0)
     LIMIT 1;";
