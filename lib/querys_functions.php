d83a6b070f49549e9126b32ef7fcb56c3dd89610
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-05-06 22:16:28 -0300

    update item

--- lib/querys_functions.php
+++ lib/querys_functions.php
@@ -888,7 +888,7 @@ function search_transaction_id($id_partner)
     // free result set
     mysqli_free_result($result);
   }
-  var_dump($transaction);
+  // var_dump($transaction);
   return $transaction;
 }
 
