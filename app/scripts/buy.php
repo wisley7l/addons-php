de5bf395d6ec8a2ce6fa807197be7a737bf2ab86
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-05-08 20:19:04 -0300

    update item

--- app/scripts/buy.php
+++ app/scripts/buy.php
@@ -130,8 +130,9 @@ if (empty($_POST)) {
     }
     // cria buy plan
     $date_duration = (int)$v['duration'];
+    $day_add = (int)($date_duration/2);
     $date_init = time ();
-    $date_end = $date_init + ($date_duration*30*24*60*60) + (($date_duration/2)*24*60*60);
+    $date_end = $date_init + ($date_duration*30*24*60*60) + ($day_add*24*60*60);
     $date_renovation = $date_end - (15*24*60*60);
     echo $date_init = date("Y-m-d H:i:s",$date_init);
     echo PHP_EOL;
