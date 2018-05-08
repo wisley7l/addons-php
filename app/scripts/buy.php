257c48823d11124fbdd59a45481d94b6545c0ef4
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-05-08 20:11:27 -0300

    update item

--- app/scripts/buy.php
+++ app/scripts/buy.php
@@ -137,6 +137,8 @@ if (empty($_POST)) {
     echo date("Y-m-d H:i:s",$date_end);
     echo PHP_EOL;
     echo date("Y-m-d H:i:s",$date_renovation);
+    echo PHP_EOL;
+    echo (int) 5/2;
     // echo $today + 1;
     /*
     $query =  "INSERT INTO `buy_apps` (`app_id`, `store_id`,`app_value`,
