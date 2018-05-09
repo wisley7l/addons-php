5c422a45ca70f0501dc0930181297aff0e18f680
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-05-08 23:37:15 -0300

    update item

--- lib/querys_functions.php
+++ lib/querys_functions.php
@@ -888,7 +888,7 @@ function get_apps_buy($id)
         'transaction' => $row['transaction_code'],
         'is_app' => 1
         );
-        // var_dump($item);
+        var_dump($item);
         // array_push($images, $item);
       }
       // free result set
