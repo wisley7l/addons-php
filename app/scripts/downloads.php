9d90329edef9617fc5b5d12c2efa6a61f24df1e8
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-05-20 18:21:10 -0300

    date end updadte

--- app/scripts/downloads.php
+++ app/scripts/downloads.php
@@ -51,22 +51,22 @@ if ((int)$_GET["is_app"] == 0) {
 
   var_dump($id_template);
   var_dump($templates);
-  $filepath = verify_template($templates['templates'], $id_template)["path_zip"];
+  $filepath = verify_template($templates['templates'], $id_template)["path_img"];
   var_dump($filepath);
 
   // // Process download
-  // if(file_exists($filepath)) {
-  //   header('Content-Description: File Transfer');
-  //   header('Content-Type: application/octet-stream');
-  //   header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
-  //   header('Expires: 0');
-  //   header('Cache-Control: must-revalidate');
-  //   header('Pragma: public');
-  //   header('Content-Length: ' . filesize($filepath));
-  //   flush(); // Flush system output buffer
-  //   readfile($filepath);
-  //   exit;
-  // }
+  if(file_exists($filepath)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($filepath));
+    flush(); // Flush system output buffer
+    readfile($filepath);
+    exit;
+  }
   // sendFile($filepath,0);
   // header("Location: ../history-transaction");
   // exit;
