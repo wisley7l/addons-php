03d053639e4b9c7cab2e8201c0f5dd95bff305ea
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-05-20 17:52:40 -0300

    date end updadte

--- app/scripts/downloads.php
+++ app/scripts/downloads.php
@@ -17,8 +17,33 @@ if (isset($_SESSION)) {
   // array used to set user panel parameters
   $user_login = getUserLogin($dictionary);
 }
-if ($_SESSION['login'] == false) { // if not connected
+if ($_SESSION['login'] == false OR $_SESSION['is_store'] == 0) { // if not connected
   //header("Location: ../#ERRORLOGIN");
   exit;
 }
+// $_SESSION['user_id']
+// $_SESSION['is_store']
+$id_user = (int) $_SESSION['user_id'];
+
 var_dump($_GET);
+
+if ((int)$_GET["is_app"] == 0) {
+  $conn = $GLOBALS['conn']; // get varible global conn
+  $id_buy = (int)$_GET['id'];
+  $query = "SELECT b.id, b.store_id, b.payment_status, b.license_type,
+    b.template_id, t.json_body
+    FROM buy_theme b, theme t
+    WHERE b.id = $id_buy AND b.store_id = $id_user AND b.payment_status = 1
+    AND t.id = b.theme_id;";
+
+  if ( $result = mysqli_query($conn, $query)) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      $id_template = (int) $row['template_id'];
+      $templates = $row['json_body'];
+    }
+    // free result set
+    mysqli_free_result($result);
+  }
+  echo "string";
+  var_dump($templates);
+}
