1ee8ab74f1e9159e6a401c4bf225b51b540c3e22
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-05-21 20:38:46 -0300

    edit

--- app/scripts/login-store.php
+++ app/scripts/login-store.php
@@ -5,7 +5,7 @@
 //session
 
 //createSession($id,$email,$name,$credits,$image,$is_store)
-//*
+/*
 $conn = connect_db();
 $id_store = (int) 1;
 
@@ -40,20 +40,20 @@ if ($id == NULL OR $id == '' ) {
 //*/
 //*
 $nonce = uniqid(rand(), true);
-echo $nonce;
-echo PHP_EOL;
+// echo $nonce;
+// echo PHP_EOL;
 $str_nonce = "nonce=" . $nonce;
-echo $str_nonce;
-echo PHP_EOL;
+// echo $str_nonce;
+// echo PHP_EOL;
 $paylod = base64_encode($str_nonce);
-echo $paylod;
-echo PHP_EOL;
+// echo $paylod;
+// echo PHP_EOL;
 $url_paylod = urlencode($paylod);
-echo $url_paylod;
-echo PHP_EOL;
+// echo $url_paylod;
+// echo PHP_EOL;
 $sha256 = hash_hmac('sha256',$paylod,Addons\SSO_SECRET);
-echo $sha256;
-echo PHP_EOL;
+// echo $sha256;
+// echo PHP_EOL;
 // https://admin.e-com.plus/session/sso/v1/market
 header("Location: https://admin.e-com.plus/session/sso/v1/market?sso=$url_paylod&sig=$sha256");
 // exit();
