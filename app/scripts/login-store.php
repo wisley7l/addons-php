e16557f8dd58319976949aedd22dbf14c11d4d9b
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-05-27 18:10:06 -0300

    edit

--- app/scripts/login-store.php
+++ app/scripts/login-store.php
@@ -46,7 +46,7 @@ $nonce = uniqid(rand(), true);
 // $_COOKIE['nonce'] = $nonce;
 // echo PHP_EOL;
 // echo $_COOKIE['nonce'];
-setcookie ( "nonce", $nonce, 0 , "../");
+setcookie ( "nonce", $nonce, 0 , "/");
 // echo $nonce;
 // echo PHP_EOL;
 $str_nonce = "nonce=" . $nonce;
