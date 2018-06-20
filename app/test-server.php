806699678a14c9787e06cf3bec37ae3b328ebb58
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-06-20 20:39:17 -0300

    edit

--- app/test-server.php
+++ app/test-server.php
@@ -1,29 +1,29 @@
 <?php
 // // sendFile ();
 $recipients = 'wisley7lagoas@yahoo.com.br';
-// $id_app = 1;
-// $id_partner = 10;
-// $subject = 'Sending a module-type application';
-// $text = '';
-// $msg = "<p>Hello!</p>" .
-// "<p> A module-type application was sent to the marketplace server. </p> " .
-// "<p> App ID: $id_app </p>" .
-// "<p> Partner ID: $id_partner </p>";
-// send_mailjet($subject,$text,$msg,$recipients);
+$id_app = 1;
+$id_partner = 10;
+$subject = 'Sending a module-type application';
+$text = '';
+$msg = "<p>Hello!</p>" .
+"<p> A module-type application was sent to the marketplace server. </p> " .
+"<p> App ID: $id_app </p>" .
+"<p> Partner ID: $id_partner </p>";
+send_mailjet($subject,$text,$msg,$recipients);
 
-$email = $recipients;
-// get in API
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://e-com.plus/api/v1/partners.json?email='. $email);
-// prevent execution timeout
-curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-  'Content-Type: application/json'
-));
-$response_body = curl_exec($ch);
-// all done, close cURL
-curl_close($ch);
-$json = json_decode($response_body, true);
-echo $response_body;
-// var_dump($json);
+// $email = $recipients;
+// // get in API
+// $ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL, 'https://e-com.plus/api/v1/partners.json?email='. $email);
+// // prevent execution timeout
+// curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//   'Content-Type: application/json'
+// ));
+// $response_body = curl_exec($ch);
+// // all done, close cURL
+// curl_close($ch);
+// $json = json_decode($response_body, true);
+// echo $response_body;
+// // var_dump($json);
