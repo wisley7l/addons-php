268359185b5d04d04b00791d6381693ac5605f3d
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-06-22 16:00:54 -0300

    edit

--- app/item-page.php
+++ app/item-page.php
@@ -149,8 +149,8 @@ $theme_category = get_categories_theme();
 // intial twig and send varibles for template
 $loader = new Twig_Loader_Filesystem(Addons\PATH_APP . '/views');
 $twig = new Twig_Environment($loader);
-// echo $twig->render('item-page.twig', array(
-echo $twig->render('page-test.twig', array(
+echo $twig->render('item-page.twig', array(
+// echo $twig->render('page-test.twig', array(
   'dictionary' => $dictionary,
   'login' => $_SESSION['login'],
   'info_footer' => $info_footer,
