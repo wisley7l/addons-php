eb78f3da280d37f3f8c2c5694f77903ab3420e1e
Wisley7l <wisley7lagoas@yahoo.com.br>
2018-06-22 12:44:18 -0300

    edit

--- app/test-server.php
+++ app/test-server.php
@@ -1,16 +1,16 @@
 <?php
-echo Addons\EMAIL_SEND;
+// echo Addons\EMAIL_SEND;
 // // sendFile ();
 $recipients = Addons\EMAIL_SEND;
-$id_app = 1;
-$id_partner = 10;
-$subject = 'Sending a module-type application';
-$text = '';
-$msg = "<p>Hello!</p>" .
-"<p> A module-type application was sent to the marketplace server. </p> " .
-"<p> App ID: $id_app </p>" .
-"<p> Partner ID: $id_partner </p>";
-send_mailjet($subject,$text,$msg,$recipients);
+// $id_app = 1;
+// $id_partner = 10;
+// $subject = 'Sending a module-type application';
+// $text = '';
+// $msg = "<p>Hello!</p>" .
+// "<p> A module-type application was sent to the marketplace server. </p> " .
+// "<p> App ID: $id_app </p>" .
+// "<p> Partner ID: $id_partner </p>";
+// send_mailjet($subject,$text,$msg,$recipients);
 
 // $email = $recipients;
 // // get in API
